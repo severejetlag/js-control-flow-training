fcb15ce3464521d03fedcece56790e3cda326c81
severejetlag <nhl1013@gmail.com>
2017-09-27 21:50:46 -0700

    Add logic to login.js

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,22 @@
-console.log("login.js loaded");
+var userLogin = {username: "severejetlag", password: "testpass"};
+
+var isPasswordCorrect;
+var loginAttempts = 3; 
+var userPass = window.prompt("Enter password for " + userLogin.username, "password")
+
+if(userPass === userLogin.password){
+	isPasswordCorrect = true;
+	alert("Login Successful!");
+}else{
+	while (!isPasswordCorrect && loginAttempts > 0){
+		userPass = window.prompt("Wrong password for " + userLogin.username + ". You have " + loginAttempts + " attempts remaining.");
+		if(userPass === userLogin.password){
+			isPasswordCorrect = true;
+			alert("Login Successful!");
+			break;
+		}
+		loginAttempts--;
+	}
+}
+
+
