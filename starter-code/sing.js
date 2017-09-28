0ce8eb7a1e22a7b98b614464483a36ad9e6afafe
severejetlag <nhl1013@gmail.com>
2017-09-27 22:18:31 -0700

    Adding sing.js logic

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,12 @@
 console.log("sing.js loaded");
+	
+var verseCount = window.prompt("How many verses would you like?");
+
+for(var i = verseCount, verseText; i > 1; i--){
+	if((i - 1) > 1){
+		verseText = `${i} bottles of beer on the wall,\n${i} bottles of beer! \nTake one down and pass it around,\n${i - 1} bottles of beer on the wall!`;
+	}else{
+		verseText = `${i} bottles of beer on the wall,\n${i} bottles of beer! \nTake one down and pass it around,\n${i - 1} bottle of beer on the wall!`;
+	}
+	console.log(verseText);
+}
