34460090c31ee752a84faa90984895d7b8ff2c44
severejetlag <nhl1013@gmail.com>
2017-09-27 20:38:22 -0700

    Adding logic to Security Questions in security_questions.js

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,17 @@
 console.log("security_questions.js loaded");
+
+//Create array to hold question objects
+var securityQuestions = [
+	{question: "What is your dog's name?", expectedAnswer:"woofer"},
+	{question: "In what city were you born?", expectedAnswer:"sf"},
+	{question: "Where did you last work?", expectedAnswer: "google"}
+]
+
+//Select question
+for(var i = 0; i < securityQuestions.length; i++){
+	var userAnswer = window.prompt(securityQuestions[i].question);
+	if(securityQuestions[i].expectedAnswer !== userAnswer){
+		alert("Incorrect answers!");
+		break;
+	}
+}
